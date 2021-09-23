c142913b38ebc5745fd53adcec6925585ea4325e
shylesh.vijaikumar <shylesh.vijaikumar@N13194.intellectdesign.com>
2021-09-23 14:34:45 +0530

    modified class 5

--- DemoProject/src/com/shylesh/class5.java
+++ DemoProject/src/com/shylesh/class5.java
@@ -3,7 +3,8 @@ package com.shylesh;
 public class class5 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("Hey class 5");	}
+		
+System.out.println("Hey class 5");	
+System.out.print("---------");}
 
 }
