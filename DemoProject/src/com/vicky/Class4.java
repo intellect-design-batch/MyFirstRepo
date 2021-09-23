fdf46a3bddf6ecbc015fda44264afdb83373e5c8
vignesh.ramajayam <vignesh.ramajayam@N13245.intellectdesign.com>
2021-09-23 14:34:47 +0530

    modified class 4

--- DemoProject/src/com/vicky/Class4.java
+++ DemoProject/src/com/vicky/Class4.java
@@ -3,8 +3,8 @@ package com.vicky;
 public class Class4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("hello i am class 4");
+		System.out.println("modified");
 	}
 
 }
